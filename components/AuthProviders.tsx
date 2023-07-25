207d5a2a88cf5fd596134c6f915bc4b136ec9be3
Wifqo Arova Syams <56685562+ArovaSyams@users.noreply.github.com>
2023-07-25 22:07:44 +0300

    add grafbase model config authprovider layout

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+"use client"
+
+import {  getProviders, signIn } from 'next-auth/react';
+import { useState, useEffect } from 'react';
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null
+}
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return (
-    <div>AuthProviders</div>
-  )
+  const [providers, setProviders] = useState<Providers | null>(null)
+
+  useEffect(() => {
+    const fetchProvider = async () => {
+      const res = await getProviders();
+
+      setProviders(res);
+    }
+  
+    fetchProvider();
+  }, []);
+  
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider, i) => (
+          <button key={i}>{provider.name}</button>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default AuthProviders
